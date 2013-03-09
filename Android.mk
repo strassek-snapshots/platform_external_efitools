7ec0f79d63713861bfb4beb269b9ad14400782b4
Jianxun Zhang <jianxun.zhang@intel.com>
2013-03-08 16:55:11 -0800

    Bring efitools (64-bit) to secure boot chain
    () Provide Makefiles and script to generate 64-bit builds in IA tree.
    Make*.android are derived from original GNU Makefiles.
    
    () Use prebuilt key pairs to replace auto-generated ones by openssl
    at build time. The prebuilt images will be signed with these static
    key pairs consistently.
    
    () Naming conventions:
    *.unsigned.efi : unsigned efi images
    *.efi : SIGNED efi images
    
    For: AXIA-1738
    Change-Id: I6ba5bbf0644fdca8c60a291292a3367417cf8b64
    Signed-off-by: Jianxun Zhang <jianxun.zhang@intel.com>

---
+++ Android.mk
@@ -0,0 +1,22 @@
+# derived from external/uefi_shim.
+
+LOCAL_PATH := $(call my-dir)
+
+# TODO: support ia32 prebuilt
+
+ifeq ($(TARGET_KERNEL_ARCH),x86_64)
+
+arch_name := x86_64
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := LockDown.efi
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(PRODUCT_OUT)/efi
+LOCAL_MODULE_STEM := $(LOCAL_MODULE)
+LOCAL_SRC_FILES := ../../prebuilts/tools/linux-$(arch_name)/efitools/LockDown.efi
+include $(BUILD_PREBUILT)
+
+LOCKDOWN_EFI := $(PRODUCT_OUT)/efi/LockDown.efi
+
+endif
---
+++ generate-prebuilts.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+SUPPORTED_ARCH="x86_64"
+
+if [ ! -d "$ANDROID_BUILD_TOP" ] ; then
+    echo "Android top dir is invalid. Did you forget to run these commands first?"
+    echo " . ./build/envsetup.sh"
+    echo " lunch "
+    exit 1
+fi
+
+MANUAL_RECOVER="You need to recover the source directory by hand and build again."
+
+CC=$ANDROID_BUILD_TOP/prebuilts/gcc/linux-x86/host/x86_64-linux-glibc2.7-4.6/bin/x86_64-linux-gcc
+
+for arch in $SUPPORTED_ARCH ; do
+    if [ $arch == "ia32" ] ; then
+        ARCH_SUFFIX=x86
+    else
+        ARCH_SUFFIX=$arch
+    fi
+
+    PREBUILTS_PATH=$ANDROID_BUILD_TOP/prebuilts/tools/linux-$ARCH_SUFFIX/efitools
+
+    MAKE_ANDROID_CMD="make CC=$CC ARCH=$arch -f Makefile.android"
+
+    if ! $MAKE_ANDROID_CMD clean ; then
+        echo "make clean failed. $MANUAL_RECOVER"
+        exit 1
+    fi
+
+    if ! $MAKE_ANDROID_CMD ; then
+        echo "make failed. $MANUAL_RECOVER"
+        exit 1
+    fi
+
+    if ! mkdir -p $PREBUILTS_PATH ; then
+        echo "Could not create prebuilt path: $PREBUILTS_PATH"
+        exit 1
+    fi
+
+    if ! cp $PWD/*.efi $PREBUILTS_PATH/ ; then
+        echo "Failed to copy images to $PREBUILTS_PATH"
+        exit 1
+    fi
+
+    if ! $MAKE_ANDROID_CMD clean ; then
+        echo "Failed to clean source directory after build. $MANUAL_RECOVER"
+        exit 1
+    fi
+
+    echo "Installed efitools in $PREBUILTS_PATH"
+
+done
+
+echo "Installed efitools in all supported architectures."
